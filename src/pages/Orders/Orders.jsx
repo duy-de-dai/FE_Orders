70e1b51ce8f08e1d6fe35c7334ca301616b47050
duy-de-dai <maivuduy2k2@gmail.com>
2023-02-12 11:16:17 +0700

    nhaps

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, Search, Filter, Page, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
 import './Orders.css';
 import { registerLicense } from '@syncfusion/ej2-base';
-
+import { closest } from '@syncfusion/ej2-base';
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 
 registerLicense('Mgo+DSMBaFt/QHRqVVhkVFpHaV5AQmFJfFBmRmlbeVRxdEU3HVdTRHRcQl9iSX5QdENiWH1cd3c=;Mgo+DSMBPh8sVXJ0S0J+XE9AflRBQmJLYVF2R2BJfFRycl9HYEwgOX1dQl9gSXxSdkVgW3lfcXZRQ2M=;ORg4AjUWIQA/Gnt2VVhkQlFacldJXnxIfEx0RWFab116dlNMYVxBNQtUQF1hSn5Rd0VjWn1YcXVXQGFf;MTEwMzU1MUAzMjMwMmUzNDJlMzBqR2R4RXlIS0pzZHBITXJEZm9sQW8xOThnUjZDc1JTZU9iV0k3Rk9raWZNPQ==;MTEwMzU1MkAzMjMwMmUzNDJlMzBrek5FQzdkWG5qV01lS2VRSXpBc1A1OTNuRUl4US9VWWVQQ29ZN3NRZ0pVPQ==;NRAiBiAaIQQuGjN/V0Z+WE9EaFtKVmBWf1FpR2NbfE5zfldAal1RVAciSV9jS31TdERjWXxcdnVURWJeUg==;MTEwMzU1NEAzMjMwMmUzNDJlMzBPVWtoTkQzQm1XQlNIN1IvaU9qdXBkalIwRGE4Myt4bjBUSGsrc20xZEhvPQ==;MTEwMzU1NUAzMjMwMmUzNDJlMzBoUTV1Z2NSYzhBVWdSWDNJRDVoQ0h2YVNXNC94dzNrV0I2MzVMSTdEMWg0PQ==;Mgo+DSMBMAY9C3t2VVhkQlFacldJXnxIfEx0RWFab116dlNMYVxBNQtUQF1hSn5Rd0VjWn1YcXVWT2Nf;MTEwMzU1N0AzMjMwMmUzNDJlMzBsaWY4a0V4TG9pd1RRem10d3JnRlRqQVlzcWMxdmpKazBOK0lSZUNzYXhFPQ==;MTEwMzU1OEAzMjMwMmUzNDJlMzBUQUhUM2gyY2k1bDVMTUNJM3p5Ry9rYzdzV0xZZ0lscnk3bkVoMThNamFZPQ==;MTEwMzU1OUAzMjMwMmUzNDJlMzBPVWtoTkQzQm1XQlNIN1IvaU9qdXBkalIwRGE4Myt4bjBUSGsrc20xZEhvPQ==');
@@ -10,6 +10,19 @@ registerLicense('Mgo+DSMBaFt/QHRqVVhkVFpHaV5AQmFJfFBmRmlbeVRxdEU3HVdTRHRcQl9iSX5
 const Orders = () => {
   
   const toolbarOptions = ['Search'];
+  let grid;
+  const gridTemplate = (props) => {
+    
+      return (<div>
+          <button className="empData bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Employee Data</button>
+      </div>);
+  };
+  const recordClick = (args) => {
+      if (args.target.classList.contains('empData')) {
+          let rowObj = grid.getRowObjectFromUID(closest(args.target, '.e-row').getAttribute('data-uid'));
+          console.log(rowObj);
+      }
+  };
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <div className=" mb-10">
@@ -19,18 +32,24 @@ const Orders = () => {
         </p>
       </div>
       <GridComponent
-        id="gridcomp"
+
         dataSource={ordersData}
         allowPaging
         allowSorting
         toolbar={toolbarOptions}
         
-       
+        recordClick={recordClick}
+        ref={g => grid = g}
         
       >
         <ColumnsDirective>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+         
           {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+          
+          <ColumnDirective headerText='Employee Data' width='150' template={gridTemplate} textAlign='Right' isPrimaryKey='true'/>
+          
+          
+          
         </ColumnsDirective>
         <Inject services={[Resize, Sort, Filter,Toolbar , Page]} />
       </GridComponent>
--- src/pages/data/dummy.js
+++ src/pages/data/dummy.js
@@ -42,6 +42,7 @@ export const gridOrderStatus = (props) => (
     style={{ background: props.StatusBg }}
     className="text-white py-1 px-2 capitalize rounded-2xl text-md"
   >
+    
     {props.Status}
     
   </button>
